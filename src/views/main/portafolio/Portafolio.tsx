3e53a8c85b85505674792b58732f0da49f7b1d97
Samuel <sammyeselpro@gmail.com>
2026-02-24 17:50:13 -0500

    PUTA MIERDAAA

--- src/views/main/portafolio/Portafolio.tsx
+++ src/views/main/portafolio/Portafolio.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import imgAbogados from '../assets/portafolio/abogados.png';
-import imgDusklight from '../assets/portafolio/dusklight.png';
-import imgProvedor from '../assets/portafolio/provedor.png';
-import imgSunny from '../assets/portafolio/sunny.png';
-import imgBackend from '../assets/portafolio/backend.png';
+import imgAbogados from '../../../assets/portafolio/abogados.png';
+import imgDusklight from '../../../assets/portafolio/dusklight.png';
+import imgProvedor from '../../../assets/portafolio/provedor.png';
+import imgSunny from '../../../assets/portafolio/sunny.png';
+import imgBackend from '../../../assets/portafolio/backend.png';
 
 interface Proyecto {
   id: number;
